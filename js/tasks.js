80800220baf2d44f9c97b6575333cc0d0de8231e
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-07-28 22:07:43 +0200

    Create tasks.js

---
+++ js/tasks.js
@@ -0,0 +1,36 @@
+1
+// const btn = document.querySelector('#btn')
+// btn.addEventListener('click', message)
+// function message() {
+//    console.log( 'Button clicked!');
+// }
+2
+// const p = document.querySelector('p')
+// const btn = document.querySelector('#btn')
+
+// btn.addEventListener('click', newText)
+// function newText() {
+//  p.textContent = 'Text changed!'
+// }
+3
+// const btn = document.querySelector('#btn')
+// const conter = document.querySelector('#counter')
+// let count = 0;
+// btn.addEventListener('click', nomberUP)
+// function nomberUP() {
+//     count += 1;
+//     counter.textContent = count;
+// }
+4
+// const div = document.querySelector('.highlight')
+// div.addEventListener('click', removeClass)
+// function removeClass() {
+//     div.remove()
+// }
+5
+// const input = document.querySelector('#input')
+// const output = document.querySelector(".output");
+// input.addEventListener('input', outText)
+// function outText(ev) {
+//     output.textContent = ev.currentTarget.value;
+// }
