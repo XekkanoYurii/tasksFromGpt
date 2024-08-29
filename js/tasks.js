8c73b050af87e95dd0aa06efb7a26f89b877afaa
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-08-29 16:25:58 +0200

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -38,11 +38,50 @@
 
 
 
-console.log ("log");
-function foo(x, n) {
-    let arr = [];
-    for (let i = 1; i <= n; i++)
-    arr.push(x * i)
-    return arr
-}
-console.log(foo(2, 1));
+// console.log ("log");
+// function foo(x, n) {
+//     let arr = [];
+//     for (let i = 1; i <= n; i++)
+//     arr.push(x * i)
+//     return arr
+// }
+// console.log(foo(2, 1));
+
+
+
+//1...... Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів.
+
+
+
+// Оголоси функцію makeTransaction, яка очікує два параметри, значення яких будуть задаватися під час її виклику: • quantity— перший параметр, число, що містить кількість замовлених дроїдів • pricePerDroid — другий параметр, число, що містить вартість одного дроїда
+
+
+
+// Доповни код функції так, щоб вона повертала рядок з повідомленням про покупку ремонтних дроїдів: "You ordered <quantity> droids worth <totalPrice> credits!", де: • <quantity> — це кількість замовлених дроїдів • <totalPrice> — це загальна вартість замовлення, тобто вартість усіх замовлених дроїдів
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+// console.log(makeTransaction(25, 15));
+
+//2.... Оголоси функцію getShippingMessage, яка очікує три параметри, значення яких будуть задаватися під час її виклику: • country — перший параметр, рядок, що містить країну доставки • price — другий параметр, число, що містить загальну вартість товару • deliveryFee — третій параметр, число, що містить вартість доставки товару
+
+
+
+// Доповни код функції так, щоб вона повертала рядок з повідомленням про доставку товару в країну користувача: "Shipping to <country> will cost <totalPrice> credits", де: • <country> — це країни доставки • <totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки
+
+//     Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// function getShippingMessage(country, price, deliveryFee) {
+//     const totalPrice = price + deliveryFee
+//         return `Shipping to ${country} will cost ${totalPrice} credits`
+// }
+// console.log(getShippingMessage("Alla", 20, 40));
+
+
+
+// 3 
